0a76c4924a0c5794b4b4d06e949e5dd01cc5eefd
debz-g <debz.exe@gmail.com>
2023-04-15 18:05:51 +0530

    Added fonts, colors, all necessary dependencies

--- app/src/main/java/dev/redfox/planetpulse/GetStartedActivity.kt
+++ app/src/main/java/dev/redfox/planetpulse/GetStartedActivity.kt
@@ -1,11 +1,30 @@
 package dev.redfox.planetpulse
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import dev.redfox.planetpulse.databinding.ActivityGetStartedBinding
+import dev.redfox.planetpulse.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+private lateinit var binding: ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_get_started)
+        binding = ActivityGetStartedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var uri: Uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.introvideo)
+        binding.videoView.layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        binding.videoView.setVideoURI(uri)
+        binding.videoView.start()
+        binding.videoView.setOnPreparedListener { it.isLooping = true }
+
+        binding.btnGetStarted.setOnClickListener {
+
+        }
     }
 }
